69d9fccd1eb5bcc5203719c73c5f4561c9500a6f
Viktor Befort <v.befort@googlemail.com>
2015-05-27 13:49:48 +0200

    change definition of sorted pointList

--- segmentList.thy
+++ segmentList.thy
@@ -2,7 +2,6 @@ theory segmentList
 imports line
 begin
 
-(*nach alternative in der Isabelle-Library suchen*)
 lemma inInsort : "a \<in> set (insort_insert x xs) \<longleftrightarrow> a \<in> set (xs) \<or> a = x"
 by (auto simp add: List.linorder_class.set_insort_insert)
 
@@ -28,27 +27,92 @@ lemma distVertex : "pointList P \<Longrightarrow> \<forall> a b c d::point2d. a 
   apply (auto simp add: segment_Same_def distEdge)
 done
 
+(*Definiere ordnung von pointList nach x-Coord und nach y-Coord.*)
+definition xCoordSorted :: "point2d list \<Rightarrow> point2d list" where
+"xCoordSorted P \<equiv> sort_key (getX) P"
+definition yCoordSorted :: "point2d list \<Rightarrow> point2d list" where
+"yCoordSorted P \<equiv> sort_key (getY) P"
+
+(*alle xCoordinaten sind in der neuen Liste*)
+lemma inXCoord : "a \<in> set xs \<longrightarrow> a \<in> set (xCoordSorted xs)"
+  by (auto simp add: xCoordSorted_def)
+
+lemma inYCoord : "a \<in> set xs \<longrightarrow>  a \<in> set (yCoordSorted xs)"
+  by (auto simp add: yCoordSorted_def)
+  
+(*xCoordSorted gibt eine sortierte Liste zurück*)
+lemma xCoordSorted1 :  "sorted (map getX (xCoordSorted xs))"
+  apply (induct xs)
+  apply (auto simp:sorted_insort_key xCoordSorted_def)
+by (metis getX_def map_eq_conv sort_key_simps(2) sorted_sort_key)
+theorem xCoordSorted2 :  "xCoordSorted (x # xs) = (x # xs) \<longleftrightarrow> (sorted (map getX (xCoordSorted xs)) \<and> (\<forall> y \<in> set (xCoordSorted xs). getX x \<le> getX y))"
+  apply (auto simp add: xCoordSorted1)
+  apply (induct xs)
+  apply (auto simp:sorted_insort_key xCoordSorted_def)
+sorry
+lemma yCoordSorted1 :  "sorted (map getY (yCoordSorted xs))"
+  apply (induct xs)
+  apply (auto simp:sorted_insort_key yCoordSorted_def)
+by (metis getY_def map_eq_conv sort_key_simps(2) sorted_sort_key)
+theorem yCoordSorted2 :  "yCoordSorted (x # xs) = (x # xs) \<longleftrightarrow> (sorted (map getY (yCoordSorted xs)) \<and> (\<forall> y \<in> set (yCoordSorted xs). getY x \<le> getY y))"
+sorry
+
+lemma xCoordOrd1 : "pointList L \<Longrightarrow> xCoord (last (xCoordSorted L)) \<ge> xCoord (hd (xCoordSorted L))"
+  apply (cases L)
+  apply (simp add: pointList_def)
+sorry
+
+lemma yCoordOrd1 : "pointList L \<Longrightarrow> yCoord (last (yCoordSorted L)) \<ge> yCoord (hd (yCoordSorted L))"
+  apply (cases L)
+  apply (simp add: pointList_def)
+sorry
+
+
+
+
+
+
+(*alte Definition*)
+
+(*brauche ich die equivalente beschreibung mit insort_key noch?*)
+(*fun xCoordSort :: "point2d list \<Rightarrow> point2d list" where
+  "xCoordSort [] = []"
+  | "xCoordSort (x#xs) = insort_key xCoord x (xCoordSort xs)" 
+fun yCoordSort :: "point2d list \<Rightarrow> point2d list" where
+  "yCoordSort [] = []"
+  | "yCoordSort (x#xs) = insort_key yCoord x (yCoordSort xs)" 
+lemma xCoordSort_right : "xCoordSorted P = xCoordSort P"
+  apply (simp add: xCoordSorted_def)
+  apply (induction P rule: xCoordSort.induct, simp)
+  apply (simp)
+done
+lemma yCoordSort_right : "yCoordSorted P = yCoordSort P"
+  apply (simp add: yCoordSorted_def)
+  apply (induction P rule: yCoordSort.induct, simp)
+  apply (simp)
+done*)
+
 (*for trapezoidalmap. Sollte noch mit Lambda abstrakter definiert werden*)
 (*insort_insert damit neue Liste distinct ist*)
-fun yCoordList ::  "point2d list \<Rightarrow> real list" where
+(*fun yCoordList ::  "point2d list \<Rightarrow> real list" where
 "yCoordList [] = []" 
 | "yCoordList (x#xs) = insort_insert (getY x) (yCoordList xs)"
 fun xCoordList ::  "point2d list \<Rightarrow> real list" where
 "xCoordList [] = []" 
-| "xCoordList (x#xs) = insort_insert (getX x) (xCoordList xs)"
+| "xCoordList (x#xs) = insort_insert (getX x) (xCoordList xs)"*)
 
 (*xCoordList gibt eine sortierte Liste zurück*)
-lemma XCoordSorted : "sorted (xCoordList P)"
+(*lemma XCoordSorted : "sorted (xCoordList P)"
   apply (induct P rule: xCoordList.induct, simp)
   apply (simp add: sorted_insort_insert)
 done
 lemma YCoordSorted : "sorted (yCoordList P)"
   apply (induct P rule: yCoordList.induct, simp)
   apply (simp add: sorted_insort_insert)
-done
+done*)
 
 (*alle xCoordinaten sind in der neuen Liste*)
-lemma inXCoord : "a \<in> set xs \<longrightarrow> (getX a) \<in> set (xCoordList xs)"
+(*lemma inXCoord : "a \<in> set xs \<longrightarrow> (getX a) \<in> set (xCoordList xs)"
   apply (rule impI)
   apply (induct xs rule: yCoordList.induct, simp)
   apply (simp, erule disjE, simp)
@@ -61,18 +125,18 @@ lemma inYCoord : "a \<in> set xs \<longrightarrow> (getY a) \<in> set (yCoordLis
   apply (simp, erule disjE, simp)
   apply (simp add: inInsort)
   apply (simp add: inInsort)
-done
+done*)
 (*xCoordList gibt eine sortierte Liste zurück*)
-theorem XCoordSorted1 : "sorted (xCoordList (x # xs)) = (sorted (xCoordList xs) \<and> (\<forall> y \<in> set (xCoordList xs). getX x \<le> y))"
+(*theorem XCoordSorted1 : "sorted (xCoordList (x # xs)) = (sorted (xCoordList xs) \<and> (\<forall> y \<in> set (xCoordList xs). getX x \<le> y))"
   apply (auto)
   apply (simp add: XCoordSorted)
 sorry
 theorem YCoordSorted1 : "sorted (yCoordList (x # xs)) = (sorted (yCoordList xs) \<and> (\<forall> y \<in> set (yCoordList xs). getY x \<le> y))"
   apply (simp)
-sorry
+sorry*)
 
 (*Liste ist sortiert*)
-lemma xCoordOrd1 : "pointList L \<Longrightarrow> last (xCoordList L) \<ge> hd (xCoordList L)"
+(*lemma xCoordOrd1 : "pointList L \<Longrightarrow> last (xCoordList L) \<ge> hd (xCoordList L)"
   apply (cases L rule: xCoordList.cases)
   apply (simp add: pointList_def)
   apply (cut_tac P=L in YCoordSorted)
@@ -88,9 +152,7 @@ lemma yCoordOrd1 : "pointList L \<Longrightarrow> last (yCoordList L) \<ge> hd (
   apply (cut_tac P=L in XCoordSorted)
   apply (simp)
   apply (metis XCoordSorted XCoordSorted1 getX_def inInsort select_convs(1) xCoordList.simps(2))
-done
-
-
+done*)
 
 
 
--- simpTrapezoidal.thy
+++ simpTrapezoidal.thy
@@ -1,8 +1,79 @@
 theory simpTrapezoidal
-imports polygon "~~/src/HOL/Hoare/Hoare_Logic"
+imports polygon
 begin
 
 (*4eckige Box um pointListe herum ist selbst eine pointList*)
+lemma rBoxPointList: "pointList L \<Longrightarrow> 
+ pointList ([\<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>,
+  \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>, \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>,
+  \<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>])"
+sorry
+(*4eckige Box um pointListe herum*)
+definition rBox :: "point2d list \<Rightarrow> point2d list" where
+"pointList L \<Longrightarrow> rBox L \<equiv> polygon([\<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>,
+  \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>, \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>,
+  \<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>])"
+
+(*ersetzte den Term Polygon im Satz*)
+lemma rBoxPoly [simp] : "pointList L \<Longrightarrow>
+  polygon([\<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>,
+  \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>, \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>,
+  \<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>])
+  \<equiv> [\<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>,
+  \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>, \<lparr>xCoord = getX (last (xCoordSorted L)) + 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>,
+  \<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (last (yCoordSorted L)) + 1\<rparr>, \<lparr>xCoord = getX (hd (xCoordSorted L)) - 1, yCoord = getY (hd (yCoordSorted L)) - 1\<rparr>]"
+  apply (cut_tac L=L in rBoxPointList, assumption)
+  apply (auto simp add: rBox_def polygon_def)
+done
+
+(*rBox ist ein Convexes Polygon*)
+lemma rBoxConvex : "pointList L \<Longrightarrow> conv_polygon (rBox L)"
+  apply (cut_tac L=L in rBoxPointList, assumption)
+  apply (simp add: rBox_def)
+  apply (simp add: conv_polygon_def) 
+  apply (rule disjI2)
+  apply (simp add: rBox_def polygon_def)
+  apply (auto simp add: signedArea_def)
+  apply (subgoal_tac "xCoord (last (xCoordSorted L)) - xCoord (hd (xCoordSorted L)) \<ge> 0")
+  apply (subgoal_tac "yCoord (last (yCoordSorted L)) - yCoord (hd (yCoordSorted L)) \<ge> 0", simp)
+  apply (auto simp add:  le_diff_eq yCoordOrd1 xCoordOrd1)
+  apply (subgoal_tac "xCoord (last (xCoordSorted L)) - xCoord (hd (xCoordSorted L)) \<ge> 0")
+  apply (subgoal_tac "yCoord (hd (yCoordSorted L)) - yCoord (last (yCoordSorted L)) \<le> 0")
+  apply (simp add: mult_less_0_iff)
+  apply (auto simp add: yCoordOrd1 le_diff_eq xCoordOrd1)
+  apply (subgoal_tac "xCoord (last (xCoordSorted L)) - xCoord (hd (xCoordSorted L)) \<ge> 0")
+  apply (subgoal_tac "yCoord (last (yCoordSorted L)) - yCoord (hd (yCoordSorted L)) \<ge> 0", simp)
+  apply (auto simp add: le_diff_eq yCoordOrd1 xCoordOrd1)
+done
+
+(*alle Punkte von L sind innerhalb von rBox L*)
+lemma "pointList L \<Longrightarrow> \<forall> a \<in> set L. insidePolygonACl (rBox L) a"
+  apply (cut_tac L=L in rBoxConvex, assumption)
+  apply (simp add: insidePolygonACl_def)
+  apply (rule impI)
+  apply (simp add: rBox_def)
+  
+
+(*Welche TrapezStrecken schneiden das Segment*)
+(*ich brauche doch ein segment-datentypen?*)
+fun trapezInter :: "point2d list \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> real list" where
+  "trapezInter [] A B = []"
+ | " trapezInter [p] A B = []"
+  | "trapezInter (p#r#xs) A B = (if (segment A B \<and> intersect A B p r) then (getX p # trapezInter xs A B) else trapezInter xs A B)"
+
+(*S = Menge von Strecken. keine gleiche x-Koordinate. Strecken Kreuzugsfrei*)
+(*fun trapezoidalMap ::  "point2d list \<Rightarrow> real list" where
+"trapezoidalMap [] = []"
+| "trapezoidalMap (x#xs) = insort_insert (getX x) (trapezoidalMap xs)"*)
+
+
+
+
+
+
+
+(*alte Definition!*)
+(*(*4eckige Box um pointListe herum ist selbst eine pointList*)
 lemma rBoxPointList: "pointList L \<Longrightarrow> 
   pointList ([\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr> ,\<lparr>xCoord = last (xCoordList L) + 1, yCoord = hd (yCoordList L) - 1\<rparr>,
   \<lparr>xCoord = last (xCoordList L) + 1, yCoord = last (yCoordList L) + 1\<rparr>,\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = last (yCoordList L) + 1\<rparr>])"
@@ -23,25 +94,24 @@ definition rBox :: "point2d list \<Rightarrow> point2d list" where
 "pointList L \<Longrightarrow> rBox L \<equiv> polygon([\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr>
  ,\<lparr>xCoord = last (xCoordList L) + 1, yCoord = hd (yCoordList L) - 1\<rparr>, \<lparr>xCoord = last (xCoordList L) + 1, yCoord = last (yCoordList L) + 1\<rparr>,
 \<lparr>xCoord = hd (xCoordList L) - 1, yCoord = last (yCoordList L) + 1\<rparr>])"
-
-(*ersetzte den Term Polygon im Satz. Funktioniert noch nicht!*)
-lemma rBoxPoly : "pointList L \<Longrightarrow>
+*)
+(*(*ersetzte den Term Polygon im Satz*)
+lemma rBoxPoly [simp] : "pointList L \<Longrightarrow>
   (polygon([\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr>,\<lparr>xCoord = last (xCoordList L) + 1, yCoord = hd (yCoordList L) - 1\<rparr>,
   \<lparr>xCoord = last (xCoordList L) + 1, yCoord = last (yCoordList L) + 1\<rparr>,\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = last (yCoordList L) + 1\<rparr>]))
-  = [\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr> ,\<lparr>xCoord = last (xCoordList L) + 1, yCoord = hd (yCoordList L) - 1\<rparr>,
+  \<equiv> [\<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr> ,\<lparr>xCoord = last (xCoordList L) + 1, yCoord = hd (yCoordList L) - 1\<rparr>,
   \<lparr>xCoord = last (xCoordList L) + 1, yCoord = last (yCoordList L) + 1\<rparr>, \<lparr>xCoord = hd (xCoordList L) - 1, yCoord = last (yCoordList L) + 1\<rparr>,
   \<lparr>xCoord = hd (xCoordList L) - 1, yCoord = hd (yCoordList L) - 1\<rparr>]"
   apply (cut_tac L=L in rBoxPointList, assumption)
   apply (auto simp add: rBox_def polygon_def)
 done
-
-(*rBox ist ein Convexes Polygon*)
+*)
+(*(*rBox ist ein Convexes Polygon*)
 lemma rBoxConvex : "pointList L \<Longrightarrow> conv_polygon (rBox L)"
   apply (cut_tac L=L in rBoxPointList, assumption)
   apply (simp add: rBox_def)
   apply (simp add: conv_polygon_def)  
   apply (rule disjI2)
-  apply (subst rBoxPoly, assumption)
   apply (auto simp add: signedArea_def)
   apply (subgoal_tac "last (xCoordList L) - hd (xCoordList L) \<ge> 0")
   apply (subgoal_tac "last (yCoordList L) - hd (yCoordList L) \<ge> 0", simp)
@@ -54,36 +124,5 @@ lemma rBoxConvex : "pointList L \<Longrightarrow> conv_polygon (rBox L)"
   apply (subgoal_tac "last (yCoordList L) - hd (yCoordList L) \<ge> 0", simp)
   apply (auto simp add: le_diff_eq yCoordOrd1 xCoordOrd1)
 done
-  
-(*alle Punkte von L sind innerhalb von rBox L*)
-lemma "pointList L \<Longrightarrow> VARS (xs :: point2d list) i {pointList L}
-  xs := [];
-  i := 0;
-  WHILE i \<noteq> length L
-  INV {\<forall> j. 0 \<le> j \<and> j \<le> i  \<longrightarrow> insidePolygonACl (rBox L) (L!i)}
-  DO
-    i := i + 1
-  OD
-  {\<forall> j. 0 \<le> j \<and> j \<le> i  \<longrightarrow> insidePolygonACl (rBox L) (L!i)}"
-  apply(vcg_simp, auto)
-  apply (cut_tac L=L in rBoxConvex, assumption)
-  apply (simp add: insidePolygonACl_def)
-  apply (simp add: rBox_def)
-  apply (cut_tac L=L in  rBoxPoly, assumption)
-  apply (simp)
-oops
-
-(*Welche TrapezStrecken schneiden das Segment*)
-(*ich brauche doch ein segment-datentypen?*)
-fun trapezInter :: "point2d list \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> real list" where
-  "trapezInter [] A B = []"
- | " trapezInter [p] A B = []"
-  | "trapezInter (p#r#xs) A B = (if (segment A B \<and> intersect A B p r) then (getX p # trapezInter xs A B) else trapezInter xs A B)"
-
-(*S = Menge von Strecken. keine gleiche x-Koordinate. Strecken Kreuzugsfrei*)
-(*fun trapezoidalMap ::  "point2d list \<Rightarrow> real list" where
-"trapezoidalMap [] = []"
-| "trapezoidalMap (x#xs) = insort_insert (getX x) (trapezoidalMap xs)"*)
-
-
+*)
 end
