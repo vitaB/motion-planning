296133bdf1d62ce5b0eaddeb693f5881e6aebd19
Viktor Befort <v.befort@googlemail.com>
2015-06-23 18:36:52 +0200

    dokumentation und codefix

--- line.thy
+++ line.thy
@@ -3,38 +3,34 @@ imports point
 begin
 
 (*Definition für Segment.*)
-(*Evtl. wäre ein eigener Datentyp besser*)
 definition segment :: "point2d \<Rightarrow> point2d  \<Rightarrow> bool" where
 "segment a b \<equiv> \<not> pointsEqual a b"
-(*dieses lemma nicht zu simp hinzufügen!*)
 lemma segment_Sym: "segment a b \<Longrightarrow> segment b a" by(simp add: segment_def)
-
 definition segment_Same :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
-"segment A B \<Longrightarrow> segment P R \<Longrightarrow> segment_Same A B P R \<equiv> (pointsEqual A P \<and> pointsEqual B R)"  
-  
+"segment A B \<Longrightarrow> segment P R \<Longrightarrow> segment_Same A B P R \<equiv> (pointsEqual A P \<and> pointsEqual B R)"
+   
 (*Punkt befindet sich auf segment*)
 definition point_on_segment :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
 "segment A B \<Longrightarrow> collinear p A B \<Longrightarrow> point_on_segment p A B \<equiv> min (xCoord A)(xCoord B) \<le> xCoord p \<and>
-xCoord p \<le> max (xCoord A)(xCoord B) \<and> min (yCoord A)(yCoord B) \<le> yCoord p
-\<and> yCoord p \<le> max (yCoord A)(yCoord B)"
+  xCoord p \<le> max (xCoord A)(xCoord B) \<and> min (yCoord A)(yCoord B) \<le> yCoord p \<and> yCoord p \<le> max (yCoord A)(yCoord B)"
 (*point on segment ist Symmetrisch*)
-lemma point_on_segmentSym [simp] : "segment A B \<Longrightarrow> collinear p A B \<Longrightarrow>
-  point_on_segment p A B = point_on_segment p B A"
-  apply (subgoal_tac "collinear p B A \<and> segment B A")
-  apply (simp add: point_on_segment_def)
+lemma point_on_segmentSym [simp] : "segment A B \<Longrightarrow> collinear p A B \<Longrightarrow> point_on_segment p A B = point_on_segment p B A"
+  apply (subgoal_tac "collinear p B A \<and> segment B A", simp add: point_on_segment_def)
   apply (simp add: max.commute min.commute, simp add: segment_Sym)
 done
 lemma point_on_segmentSame [simp]: "segment p B \<Longrightarrow> point_on_segment p p B"
   by (simp add: point_on_segment_def segment_Sym)
 
-(*Strecke A B "trennt" die Punkte P und R: die Endpunkte von P R liegen auf verschiedenen Seiten von AB.*)
+(*Strecke A B "trennt" die Punkte P und R, wenn die Endpunkte von P R auf verschiedenen Seiten von AB liegen.*)
 definition lineSeparate :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
   "lineSeparate A B P R \<equiv> (leftTurn A B R  \<and> rightTurn A B P ) \<or> (rightTurn A B R \<and> leftTurn A B P)"
 lemma lineSeparateEq : "lineSeparate A B P R = (signedArea A B R * signedArea A B P < 0)"
-  by (simp add: lineSeparate_def, metis mult_less_0_iff leftTurn_def leftRightTurn rightTurn_def)
+  apply (simp add: lineSeparate_def)
+  apply (metis areaContra2 collSwap colliniearRight conflictingRigthTurns mult_less_0_iff not_less_iff_gr_or_eq rightTurn_def)
+done
 (*wenn lineSeparate true, dann müssen Strecken segmente sein*)
 lemma lineSeparateSegment: "lineSeparate A B P R \<Longrightarrow> segment P R"
-  by(simp add: lineSeparate_def segment_def, metis not_less_iff_gr_or_eq leftTurn_def rightTurn_def leftRightTurn)
+  by (simp add: lineSeparate_def segment_def, metis conflictingRigthTurns)
 lemma lineSeparateSegment1: "lineSeparate A B P R \<Longrightarrow> segment A B"
   by (simp add: lineSeparate_def segment_def rightTurn_def, metis areaDoublePoint less_numeral_extra(3))
 (*Zusätzliche Lemmas*)
@@ -44,20 +40,23 @@ lemma [simp]:"\<not> lineSeparate B A A B" by (simp add: lineSeparate_def)
 (*line Separate ist symmetrisch*)
 lemma lineSeparateSym [simp]: "lineSeparate A B P R = lineSeparate B A P R"
   by (auto simp add: lineSeparate_def signedArea_def leftTurn_def rightTurn_def)
-
 lemma lineSeparateSym1[simp]: "lineSeparate A B P R = lineSeparate A B R P"
  by (auto simp add: lineSeparate_def leftTurn_def)
 (*lemma lineSeparateSym2 : "\<not>(lineSeparate A B P R \<longleftrightarrow> lineSeparate P R A B)"
-  apply (simp , rule iffI)
-  apply (simp add: lineSeparate_def signedArea_def leftTurnDiffPoints)
+  apply (simp , rule iffI, simp add: lineSeparate_def signedArea_def leftTurnDiffPoints)
   apply (safe)
 oops*)
-(*eine strecke db die auf der strecke ac steht, trennt ac*)
-lemma "segment a c \<Longrightarrow> a \<noteq> b \<Longrightarrow> b \<noteq> c \<Longrightarrow> segment d b \<Longrightarrow> collinear a b c \<Longrightarrow> \<not>collinear a b d \<Longrightarrow>
-  lineSeparate d b a c"
-by (smt2 collRotate collinearOrient collinearTransitiv leftRightTurn leftTurn_def lineSeparate_def notLeftTurn notRightTurn1 pointsEqualSame segment_def)
-lemma [dest]:"lineSeparate A B A B \<Longrightarrow> False"
-  by (simp add: lineSeparate_def)
+
+(*wenn ein Punkt von AB auf einer geraden PR (und ungleich mit Ecken), dann trennt AB die Ecken P und R*)
+(****evtl. nicht Beweisbar... für pointOnSegment sollte vielleicht eine andere Definition gesucht werden*)
+lemma point_on_segment_noRightTurn : "segment P R \<Longrightarrow> A \<noteq> P \<Longrightarrow> A \<noteq> R \<Longrightarrow> collinear A P R \<Longrightarrow>
+  point_on_segment A P R \<Longrightarrow> rightTurn A B P \<Longrightarrow> rightTurn A B R \<Longrightarrow> False"
+  apply (auto simp add: point_on_segment_def)
+sorry
+lemma point_on_segment_noRightTurn1 : "segment P R \<Longrightarrow> A \<noteq> P \<Longrightarrow> A \<noteq> R \<Longrightarrow> collinear B P R \<Longrightarrow>
+  point_on_segment B P R \<Longrightarrow> rightTurn A B P \<Longrightarrow> rightTurn A B R \<Longrightarrow> False"
+  apply (auto simp add: point_on_segment_def)
+sorry
 
 (*(echter)Schnitt zwischen Segment A B und Segment P R*)
 definition crossing ::  "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
@@ -71,13 +70,9 @@ lemma crossingSym [simp]: "crossing A B P R = crossing B A P R "
 lemma crossingSym1 [simp]: "crossing A B P R = crossing P R A B "
   by (auto simp add: crossing_def lineSeparate_def)
 lemma crossingRightTurn [dest] : "crossing A B P R \<Longrightarrow> rightTurn A B P \<and> rightTurn A B R \<Longrightarrow> False"
-  apply (simp add: crossing_def lineSeparate_def)
-  apply (metis leftRightTurn notRightTurn1)
-done
+  by (simp add: crossing_def lineSeparate_def, metis conflictingRigthTurns)
 lemma crossingLeftTurn [dest] : "crossing A B P R \<Longrightarrow> leftTurn A B P \<and> leftTurn A B R \<Longrightarrow> False"
-  apply (simp add: crossing_def lineSeparate_def)
-  apply(metis leftRightTurn notRightTurn1)  
-done
+  by (simp add: crossing_def lineSeparate_def, metis conflictingRigthTurns)
 
 (*Schnitt, auch wenn Endpunkt auf Strecke liegt*)
 definition intersect :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
@@ -91,15 +86,19 @@ lemma crossingIntersect [simp]: "crossing A B P R \<Longrightarrow> intersect A 
 done
 lemma intersectSym : "intersect A B P R = intersect B A P R" sorry
 lemma intersectSym1 : "intersect A B P R = intersect P R A B" sorry
+
+(*wenn die Strecken AB und PR sich schneiden, dann können beide Ecken von PR nicht auf der rechten Seite von AB liegen*)
 lemma intersectRightTurn : "segment A B \<Longrightarrow> segment P R \<Longrightarrow> intersect A B P R \<Longrightarrow> A \<noteq> P \<Longrightarrow> A \<noteq> R \<Longrightarrow> B \<noteq> P \<Longrightarrow> B \<noteq> R \<Longrightarrow> 
   rightTurn A B P \<and> rightTurn A B R \<Longrightarrow> False"
   apply (simp add: intersect_def)
   apply (safe)
-  apply (metis crossingRightTurn)
+  apply (metis crossing_def lineSeparate_def notRightTurn1)
   apply (metis notRightTurn1)+
-  apply (smt2 areaContra collRotate collinearOrient pointsEqualSame rightTurn_def segment_def signedAreaRotate twoPointsColl)
-  apply (smt2 areaContra2 collRotate collinearOrient leftTurn_def notRightTurn notRightTurn1 pointsEqualSame segment_def signedAreaRotate twoPointsColl)
-done 
+  apply (metis point_on_segment_noRightTurn)
+  apply (metis point_on_segment_noRightTurn1)
+done
+
+(*Zusätliche Lemmas*)
 lemma intersectNotCollinear: "segment a b \<Longrightarrow> segment c d \<Longrightarrow> intersect a b c d \<Longrightarrow>
   \<not>collinear a b c \<Longrightarrow> a \<noteq> c \<and> b \<noteq> c "
   apply (simp add: intersect_def)
@@ -110,5 +109,10 @@ lemma intersectNotCollinear1: "segment a b \<Longrightarrow> segment c d \<Longr
   apply (metis crossingLeftTurn notLeftTurn notRightTurn1)
   apply (metis notRightTurn)
 by (smt2 intersectRightTurn intersect_def leftRightTurn notRightTurn notRightTurn1 segment_Sym)
+
+
+
+
+(*Lemmas und Definitionen, die momentan nicht gebraucht werden*)
 
 end
--- point.thy
+++ point.thy
@@ -46,7 +46,8 @@ definition leftTurn :: "[point2d, point2d, point2d] \<Rightarrow> bool" where(*[
 lemma leftTurnRotate [simp]: "leftTurn b c a = leftTurn a b c"(*[1]*) by (simp add: leftTurn_def)
 lemma leftTurnRotate2 [simp]: "leftTurn b a c = leftTurn a c b"(*[1]*) by (simp add: leftTurn_def)
 lemma leftTurnDiffPoints [intro]: "leftTurn a b c \<Longrightarrow> a\<noteq>b \<and> a\<noteq>c \<and> b\<noteq>c"(*[1]*) by (auto simp add: leftTurn_def)
-(*clockwise cycle*)
+
+(*three points a, b and c make a right turn if they make an clockwise cycle:*)
 definition rightTurn :: "[point2d, point2d, point2d] \<Rightarrow> bool" where
   "rightTurn a b c \<equiv> 0 > signedArea a b c"
 lemma leftRightTurn [simp] : "leftTurn a b c = rightTurn c b a"
@@ -54,66 +55,19 @@ lemma leftRightTurn [simp] : "leftTurn a b c = rightTurn c b a"
 lemma rightTurnRotate [simp]: "rightTurn b c a = rightTurn a b c" by (simp add: rightTurn_def)
 lemma rightTurnRotate2 [simp]: "rightTurn b a c = rightTurn a c b" by (simp add: rightTurn_def)
   
-(*punkt A zwischen B und C*)
-definition midpoint :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
-"midpoint a b c = (2 * yCoord a = yCoord b + yCoord c \<and> 2 * xCoord a = xCoord b + xCoord c)"
-definition betwpoint :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
-"betwpoint a b c = (\<forall> n. n > 1 \<longrightarrow> (n * yCoord a = yCoord b + yCoord c \<and> n * xCoord a = xCoord b + xCoord c))"
-lemma swapBetween [simp]: "betwpoint a c b = betwpoint a b c" by(auto simp add: betwpoint_def)
-(*lemma notBetweenSamePoint [dest]: "betwpoint a b b \<Longrightarrow> False"
-  apply (simp add: betwpoint_def)
-  apply (erule_tac x = 2 in allE)
-  apply (auto)
-oops*)
-
 (*3 Punkte sind auf einer geraden*)
 definition collinear :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where(*[1]*)
 "collinear a b c \<equiv> ((xCoord a - xCoord b)*(yCoord b - yCoord c) = (yCoord a- yCoord b)*(xCoord b - xCoord c))"
 lemma colliniearRight : "collinear a b c = (signedArea a b c = 0)"
   apply (simp add: collinear_def signedArea_def)
-  apply (rule iffI)
-by algebra+
-lemma "midpoint a b c \<longrightarrow> collinear a b c"
-  apply (rule impI)
-  apply (simp add: midpoint_def collinear_def)
-  apply (erule conjE)
-  apply algebra
-done
-lemma betwpointCollinear [intro] : "betwpoint a b c \<longrightarrow> collinear a b c"
-  apply (rule impI)
-  apply (simp add: betwpoint_def collinear_def)
-  apply (erule_tac x = 2 in allE)
-  apply (simp)
-  apply algebra
-done
-lemma betwpointCollinear2 [intro] : "betwpoint b a c \<longrightarrow> collinear a b c"
-  apply (rule impI)
-  apply (simp add: betwpoint_def collinear_def)
-  apply (erule_tac x = 2 in allE)
-  apply (simp)
-  apply algebra
-done
-lemma betwpointCollinear3 [intro] : "betwpoint c a b \<longrightarrow> collinear a b c"
-  apply (rule impI)
-  apply (simp add: betwpoint_def collinear_def)
-  apply (erule_tac x = 2 in allE)
-  apply (simp)
-  apply algebra
-done
-(*lemma "collinear a b c \<longleftrightarrow> betwpoint a b c \<or> betwpoint b a c \<or> betwpoint c a b"
-  apply (rule iffI)
-  apply (simp add: collinear_def) apply (auto)
-  apply (simp add: collinear_def betwpoint_def)
-  apply (rule allI)
-oops*)
-
+  by (rule iffI, algebra+)
 (*degenerate cases where the points may be collinear, or equivalently, the area of the triangle they define is zero:*)
 lemma collRotate [simp]: "collinear c a b = collinear a b c"(*[1]*) by (simp add: collinear_def, algebra)
 lemma collSwap [simp]: "collinear a c b = collinear a b c"(*[1]*) by (simp add: collinear_def, algebra)
 lemma twoPointsColl [simp]: "collinear a b b"(*[1]*) by (simp add: collinear_def)
 lemma twoPointsColl2 [simp]: "collinear a a b"(*[1]*) by (simp add: collinear_def)
 
-(*lemmas for leftTurn, collinear und signedArea*)
+(*lemmas for leftTurn and rightTurn*)
 lemma notLeftTurn [simp]: "(\<not> leftTurn a c b) = (leftTurn a b c \<or> collinear a b c)"(*[1]*)
   apply (simp add:leftTurn_def del: leftRightTurn, subst colliniearRight)
   apply (auto simp add: signedArea_def mult.commute)
@@ -125,8 +79,6 @@ done
 lemma notRightTurn1 [simp]: "(\<not> rightTurn a b c) = (leftTurn a b c \<or> collinear a b c)"
   by (metis leftRightTurn leftTurnRotate2 notLeftTurn)
 lemma conflictingLeftTurns [dest]: "leftTurn a b c \<Longrightarrow> leftTurn a c b \<Longrightarrow> False"(*[1]*) by (metis notLeftTurn) 
-lemma conflictingLeftTurns2 [dest]: "leftTurn a b c \<Longrightarrow> betwpoint a b c \<Longrightarrow> False"
-  by (metis betwpointCollinear notLeftTurn swapBetween) 
 lemma conflictingLeftTurns3 [dest]: "leftTurn a b c \<Longrightarrow> collinear a b c \<Longrightarrow> False"(*[1]*)
   by (metis collSwap notLeftTurn)
 lemma conflictingRigthTurns [dest]: "rightTurn a b c \<Longrightarrow> rightTurn a c b \<Longrightarrow> False" by (metis notRightTurn) 
@@ -134,8 +86,10 @@ lemma conflictingRigthTurns1 [dest]: "rightTurn a b c \<Longrightarrow> rightTur
   by (metis leftRightTurn notLeftTurn)
 lemma conflictingRightTurns2 [dest]: "rightTurn a b c \<Longrightarrow> collinear a b c \<Longrightarrow> False"
   by (metis collSwap notRightTurn)
+
+(*lemmas for collinear und signedArea*)
 lemma notCollThenDiffPoints [intro]: "\<not>collinear a b c \<Longrightarrow> a\<noteq>b \<and> a\<noteq>c \<and> b\<noteq>c"(*[1]*) by (auto)
-lemma areaContra [dest]: " signedArea a c b < 0\<Longrightarrow> signedArea a b c < 0  \<Longrightarrow> False"(*[1]*)
+lemma areaContra [dest]: " signedArea a c b < 0 \<Longrightarrow> signedArea a b c < 0  \<Longrightarrow> False"(*[1]*)
   by (metis colliniearRight leftTurn_def less_trans notLeftTurn) 
 lemma areaContra2 [dest]: "0 < signedArea a c b\<Longrightarrow> 0 < signedArea a b c \<Longrightarrow> False"(*[1]*)
   by (metis leftTurn_def notLeftTurn) 
@@ -147,45 +101,22 @@ done
 
 
 
-
 (*move/translate point*)
+(*Fehlt noch!*)
 
 
 
 
 
-
-(*Zusätliche lemmas. alternative definition für isBetween. Nochmal anschauen!*)
-(*lemma collinearTransitiv : "a \<noteq> b \<Longrightarrow> collinear a b c \<Longrightarrow> collinear a b d \<Longrightarrow> collinear a c d"
-  apply (simp add: colliniearRight)
-  apply (cases "a = c", simp, cases "a = d", simp)
-  apply (cases "c = d", simp, cases "c = b", simp)
-  apply (cases "b = d", metis collSwap colliniearRight) 
-  apply (rule ccontr, subgoal_tac "signedArea a c d > 0 \<or> signedArea a c d < 0", safe, simp)
-  apply (simp add: signedArea_def)
-sorry
-lemma collinearOrient :"a \<noteq> b \<Longrightarrow> a \<noteq> c \<Longrightarrow>a \<noteq> d \<Longrightarrow>
-  collinear a b c \<Longrightarrow> collinear a b d \<Longrightarrow> (leftTurn a c e \<and> leftTurn a d e) \<or> (rightTurn a c e \<and> rightTurn a d e)
-  \<or> (collinear a c e \<and> collinear a d e)"
-  apply (subgoal_tac " collinear a c d", simp add: colliniearRight)
-  apply (cases "a = e")
-  apply (cases "e = c")
-  apply (cases "e = d", simp)
-sorry*)
-(*lemma notBetweenSelf [simp]: "\<not> (betwpoint a a b)"
-  apply (rule notI)
-  apply (simp add: betwpoint_def)
-  apply (cut_tac  r=a and p=b in pointsNotEqual)
-  apply (erule_tac x = 1 in allE) apply (simp)
-  apply (auto)
-oops*)
+(*Lemmas und Definitionen, die momentan nicht gebraucht werden*)
 (*Punkte sind gleich, wenn*)
 lemma pointsEqualRight: "a \<noteq> b = (\<exists> d. signedArea a b d \<noteq> 0)"
   apply (auto)
   apply (rule ccontr)
-  apply (simp)
+  apply (simp, erule_tac x=d in allE)
 oops
-definition isBetween :: "[point2d, point2d, point2d] \<Rightarrow> bool" where(*[1]*)
+(*mögliche Definitionen für isBetween. Nochmal anschauen!*)
+(*definition isBetween :: "[point2d, point2d, point2d] \<Rightarrow> bool" where(*[1]*)
 " isBetween b a c \<equiv> collinear a b c \<and> (\<exists> d. signedArea a c d \<noteq> 0) \<and>
 (\<forall> d. signedArea a c d \<noteq> 0 \<longrightarrow>
 (0 < (signedArea a b d / signedArea a c d) \<and> (signedArea a b d / signedArea a c d) < 1 ))"
@@ -213,5 +144,65 @@ sorry
 (*hier findet sledgehammer ein Fehler*)
 lemma notBetween [dest]: "\<lbrakk>betwpoint A B C;betwpoint B A C\<rbrakk>  \<Longrightarrow> False"(*[1]*) sorry
 lemma notBetween2 [dest]: "\<lbrakk>betwpoint A B C ;betwpoint C A B\<rbrakk>  \<Longrightarrow> False"(*[1]*) sorry
-lemma notBetween3 [dest]: "\<lbrakk>betwpoint B A C ;betwpoint C A B\<rbrakk> \<Longrightarrow> False"(*[1]*) sorry
+lemma notBetween3 [dest]: "\<lbrakk>betwpoint B A C ;betwpoint C A B\<rbrakk> \<Longrightarrow> False"(*[1]*) sorry*)
+
+(*
+(*punkt A zwischen B und C*)
+definition midpoint :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
+"midpoint a b c = (2 * yCoord a = yCoord b + yCoord c \<and> 2 * xCoord a = xCoord b + xCoord c)"
+definition betwpoint :: "point2d \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> bool" where
+"betwpoint a b c = (\<forall> n. n > 1 \<longrightarrow> (n * yCoord a = yCoord b + yCoord c \<and> n * xCoord a = xCoord b + xCoord c))"
+lemma swapBetween [simp]: "betwpoint a c b = betwpoint a b c" by(auto simp add: betwpoint_def)
+(*lemma notBetweenSamePoint [dest]: "betwpoint a b b \<Longrightarrow> False"
+  apply (simp add: betwpoint_def)
+  apply (erule_tac x = 2 in allE)
+  apply (auto)
+oops*)
+lemma "midpoint a b c \<longrightarrow> collinear a b c"
+  apply (rule impI, simp add: midpoint_def collinear_def)
+  by (erule conjE, algebra)
+lemma betwpointCollinear [intro] : "betwpoint a b c \<longrightarrow> collinear a b c"
+  apply (rule impI, simp add: betwpoint_def collinear_def)
+  apply (erule_tac x = 2 in allE, simp, algebra)
+done
+lemma betwpointCollinear2 [intro] : "betwpoint b a c \<longrightarrow> collinear a b c"
+  apply (rule impI, simp add: betwpoint_def collinear_def)
+  apply (erule_tac x = 2 in allE, simp, algebra)
+done
+lemma betwpointCollinear3 [intro] : "betwpoint c a b \<longrightarrow> collinear a b c"
+  apply (rule impI, simp add: betwpoint_def collinear_def)
+  apply (erule_tac x = 2 in allE, simp, algebra)
+done
+(*lemma "collinear a b c \<longleftrightarrow> betwpoint a b c \<or> betwpoint b a c \<or> betwpoint c a b"
+  apply (rule iffI, simp add: collinear_def) apply (auto)
+  apply (simp add: collinear_def betwpoint_def)
+  apply (rule allI)
+oops*)
+lemma conflictingLeftTurns2 [dest]: "leftTurn a b c \<Longrightarrow> betwpoint a b c \<Longrightarrow> False"
+  by (metis betwpointCollinear notLeftTurn swapBetween) 
+*)
+
+(*lemma collinearTransitiv : "a \<noteq> b \<Longrightarrow> collinear a b c \<Longrightarrow> collinear a b d \<Longrightarrow> collinear a c d"
+  apply (simp add: colliniearRight)
+  apply (cases "a = c", simp, cases "a = d", simp)
+  apply (cases "c = d", simp, cases "c = b", simp)
+  apply (cases "b = d", metis collSwap colliniearRight) 
+  apply (rule ccontr, subgoal_tac "signedArea a c d > 0 \<or> signedArea a c d < 0", safe, simp)
+  apply (simp add: signedArea_def)
+sorry
+lemma collinearOrient :"a \<noteq> b \<Longrightarrow> a \<noteq> c \<Longrightarrow>a \<noteq> d \<Longrightarrow>
+  collinear a b c \<Longrightarrow> collinear a b d \<Longrightarrow> (leftTurn a c e \<and> leftTurn a d e) \<or> (rightTurn a c e \<and> rightTurn a d e)
+  \<or> (collinear a c e \<and> collinear a d e)"
+  apply (subgoal_tac " collinear a c d", simp add: colliniearRight)
+  apply (cases "a = e")
+  apply (cases "e = c")
+  apply (cases "e = d", simp)
+sorry*)
+(*lemma notBetweenSelf [simp]: "\<not> (betwpoint a a b)"
+  apply (rule notI)
+  apply (simp add: betwpoint_def)
+  apply (cut_tac  r=a and p=b in pointsNotEqual)
+  apply (erule_tac x = 1 in allE) apply (simp)
+  apply (auto)
+oops*)
 end
