dd56286159a574fdf5d540d574bf2422e2a42673
Viktor Befort <v.befort@googlemail.com>
2015-07-10 18:47:44 +0200

    datei für strukturen, die ich zusätzlich brauche

---
+++ norm.thy
@@ -0,0 +1,51 @@
+theory norm
+imports polygon
+begin
+
+(*es gibt kein Punkt in der Punkt Liste, mit gleicher xCoordinate*)
+definition uniqueXCoord :: "point2d list \<Rightarrow> bool" where
+  "uniqueXCoord L \<equiv> \<forall> a b. a \<noteq> b \<longrightarrow> xCoord (L!a) \<noteq> xCoord (L!b)"
+
+(*4eckige Box um pointListe herum ist selbst eine pointList*)
+lemma rBoxPointList: "pointList (concat PL) \<Longrightarrow> pointList(
+  [Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
+  Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1,yCoord (last (yCoordSort (concat PL))) + 1)])"
+sorry
+
+(*4eckige Box um pointListen herum*)
+definition rBox :: "(point2d list) list \<Rightarrow> point2d list" where
+  "pointList (concat PL) \<Longrightarrow> uniqueXCoord (concat PL) \<Longrightarrow> rBox PL \<equiv>
+  cyclePath([Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
+  Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1,yCoord (last (yCoordSort (concat PL))) + 1)])"
+
+(*ersetzte den Term Polygon im Satz*)
+lemma rBoxPoly [simp] : "pointList (concat PL) \<Longrightarrow>
+  cyclePath([Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
+  Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1,yCoord (last (yCoordSort (concat PL))) + 1)])
+  \<equiv> [Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
+  Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
+  Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1,yCoord (last (yCoordSort (concat PL))) + 1),
+  Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1)]"
+  apply (cut_tac PL=PL in rBoxPointList, assumption)
+  apply (auto simp add: rBox_def cyclePath_def)
+done
+
+(*rBox ist ein Convexes Polygon*)
+lemma rBoxConvex : "pointList (concat PL) \<Longrightarrow> polygon (rBox PL)"  
+oops
+
+(*alle Punkte von PL sind innerhalb von rBox PL*)
+lemma "pointList (concat PL) \<Longrightarrow> \<forall> a \<in> set L. pointInsidePolygon (rBox PL) a"
+  apply (cut_tac PL=PL in rBoxConvex, assumption)
+oops
+
+
+
+end
