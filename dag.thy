68b5a9ec1bf32aecd02a21a56db2712bc586f0bc
Viktor Befort <v.befort@googlemail.com>
2015-07-10 21:15:27 +0200

    test replaceTrapez

--- dag.thy
+++ dag.thy
@@ -9,13 +9,15 @@ definition topT :: "trapez \<Rightarrow> (point2d\<times>point2d)" where  "topT 
 definition bottomT :: "trapez \<Rightarrow> (point2d\<times>point2d)" where "bottomT T \<equiv> fst(snd(Rep_trapez T))"
 definition leftP :: "trapez \<Rightarrow> point2d" where "leftP T \<equiv> fst(snd(snd(Rep_trapez T)))"
 definition rightP :: "trapez \<Rightarrow> point2d" where "rightP T \<equiv> snd(snd(snd(Rep_trapez T)))"
-lemma [simp] : "topT (Abs_trapez ((a,b),(c,d),e,f)) = (a,b)" by (simp add: topT_def Abs_trapez_inverse)
-lemma [simp] : "bottomT (Abs_trapez ((a,b),(c,d),e,f)) = (c,d) "by (simp add: bottomT_def Abs_trapez_inverse)
-lemma [simp] : "leftP (Abs_trapez ((a,b),(c,d),e,f)) = e" by (simp add: leftP_def Abs_trapez_inverse)
-lemma [simp] : "rightP (Abs_trapez ((a,b),(c,d),e,f)) = f" by (simp add: rightP_def Abs_trapez_inverse)
-lemma trapezSameCoord : "(Abs_trapez ((a,b),(c,d),e,f) = Abs_trapez ((a',b'),(c',d'),e',f'))
+lemma topTSimp [simp] : "topT (Abs_trapez ((a,b),(c,d),e,f)) = (a,b)" by (simp add: topT_def Abs_trapez_inverse)
+lemma bottomTSimp [simp] : "bottomT (Abs_trapez ((a,b),(c,d),e,f)) = (c,d) "by (simp add: bottomT_def Abs_trapez_inverse)
+lemma leftPSimp [simp] : "leftP (Abs_trapez ((a,b),(c,d),e,f)) = e" by (simp add: leftP_def Abs_trapez_inverse)
+lemma rightPSimp [simp] : "rightP (Abs_trapez ((a,b),(c,d),e,f)) = f" by (simp add: rightP_def Abs_trapez_inverse)
+lemma trapezSameCoord [simp]: "(Abs_trapez ((a,b),(c,d),e,f) = Abs_trapez ((a',b'),(c',d'),e',f'))
   \<longleftrightarrow> a=a'\<and> b=b' \<and> c=c' \<and> d=d' \<and> e=e' \<and> f=f'"
   by (metis Abs_trapez_inverse Collect_const UNIV_I fst_conv snd_conv)
+lemma trapezSameCoord1 : "(Abs_trapez ((a,b),(c,d),e,f) \<noteq> Abs_trapez ((a',b'),(c',d'),e',f'))
+  \<longleftrightarrow> a\<noteq>a'\<or> b\<noteq>b' \<or> c\<noteq>c' \<or> d\<noteq>d' \<or> e\<noteq>e' \<or> f\<noteq>f'" sorry
 
 (*ein Trapez und seine Nachbarn*)
 (*record trapezoid = trapez :: trapez
@@ -35,7 +37,7 @@ and belowChild() depending on whether the child item is above or below the segme
 datatype_new dag = Tip "trapez" | Node "dag" kNode "dag"
 
 
-(*Algorithm QueryTrapezoidMap( n, p)
+(*Algorithm QueryTrapezoidMap(dag,point2d)
 Input: T is the trapezoid map search structure, n is a node in the search structure and p is a query point.
 Output:  A pointer to the node in D for the trapezoid containing the point p.*)
 fun queryTrapezoidMap :: "dag \<Rightarrow> point2d \<Rightarrow> dag" where
@@ -64,7 +66,7 @@ definition replaceTrapezSimpQ :: "trapez \<Rightarrow> point2d \<Rightarrow> poi
 (*Einfacher Fall: wenn P und Q in T liegen*)
 definition replaceTrapezSimp :: "trapez \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> dag" where
   "replaceTrapezSimp  T P Q = (
-    if(leftP T \<noteq> P \<and> rightP T \<noteq> Q) (*P und Q sind keine Endpunkte von Trapezen*)
+    if((leftP T \<noteq> P \<and> rightP T \<noteq> Q)) (*P und Q sind keine Endpunkte von Trapezen*)
     then (
       Node (Tip(Abs_trapez(topT T,bottomT T,leftP T,P)))
         (xNode P)
@@ -121,10 +123,11 @@ definition replaceTrapezLast :: "trapez \<Rightarrow> trapez list \<Rightarrow> 
   (Tip (Abs_trapez(topT T,bottomT T, Q, rightP T)))
   ))"
 
+(*Algorithm replaceTrapez(dag,trapez, trapez list, segment)*)
 (*Input: SuchBaum D, Trapez T das ersetz werden soll, Trapezliste TM mit Trapezen die Strecke PQ kreuzt, Strecke PQ
 Output: dag welches Trapez T ersetzen soll*)
 definition replaceTrapez :: "dag \<Rightarrow> trapez \<Rightarrow> trapez list \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> dag" where
-"replaceTrapez D T TM P Q = (if (queryTrapezoidMap D P = queryTrapezoidMap D Q) then (replaceTrapezSimp T P Q)
+"replaceTrapez D T TM P Q = (if (length TM = 1) then (replaceTrapezSimp T P Q)
     else (if (queryTrapezoidMap D P = (Tip T)) then (replaceTrapezFirst T TM P Q)
       else (if (queryTrapezoidMap D Q = (Tip T)) then (replaceTrapezLast T TM P Q)
         else (replaceTrapezM T TM P Q)
@@ -134,14 +137,14 @@ definition replaceTrapez :: "dag \<Rightarrow> trapez \<Rightarrow> trapez list 
 (*gehe von links nach rechts durch die Trapeze, die die Strecke S schneidet.
 Input: A Trapezoidal map T, a search structure D, segment PQ
 Output: list of trapezoids intersected by PQ *)
-fun followSegment :: "trapezoid \<Rightarrow> point2d \<Rightarrow> trapezoid list" where
+fun followSegment :: "trapez \<Rightarrow> point2d \<Rightarrow> trapez list" where
   "followSegment t a = (if (pointInTrapez t a)
   then (t) else (t # followSegment(trapezRightAdjacent t) a))"
 
 (*gib eine List mit trapezen zurück die das Segment PQ schneiden
-Input: suchBaum Da, Segment PQ
+Input: suchBaum D, Segment PQ
 Output: liste mit trapezen*)
-fun intersectTrapez :: "dag \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> trapezoid list" where
+fun intersectTrapez :: "dag \<Rightarrow> point2d \<Rightarrow> point2d \<Rightarrow> trapez list" where
   "intersectTrapez D p q = (
     if (queryTrapezoidMap D p = queryTrapezoidMap D q) then ([queryTrapezoidMap D p])
     else ( followSegment (queryTrapezoidMap D (leftPSegment p q)) (rightPSegment p q) )
@@ -163,11 +166,27 @@ fun buildTrapezoidalMap :: "dag \<Rightarrow> " where
       else ())
     else (rest der trapezoidal anhängen))"*)
 
+(*überprüfe ob durch alle Ecken in TrapezoidalMap ein "Teil-slab geht"*)
 (*vertikale Strecken einzeichnen, die durch Eckpunkte gehen
 Eingabe Menge der Segmente(polygone ohne cyclePath) und rBox*)
-fun slabs :: "point2d list \<Rightarrow> point2d list \<Rightarrow> point2d list" where
+(*fun slabs :: "point2d list \<Rightarrow> point2d list \<Rightarrow> point2d list" where
   "slabs [] R  = []"
   | "slabs (x#xs) R = [Abs_point2d(xCoord x, yCoord (hd (yCoordSort R))),
-    Abs_point2d(xCoord x, yCoord (last (yCoordSort R)))] @ slabs xs R"
+    Abs_point2d(xCoord x, yCoord (last (yCoordSort R)))] @ slabs xs R"*)
 
+
+(*kleiner Test*)
+definition "t0 \<equiv> Abs_trapez ((Abs_point2d(1,3),Abs_point2d(4,4)),(Abs_point2d(0,0),Abs_point2d(3,0)),Abs_point2d(0,0),Abs_point2d(4,4))"
+definition "t1 \<equiv> Abs_trapez ((Abs_point2d(1,3),Abs_point2d(4,4)),(Abs_point2d(0,0),Abs_point2d(3,0)),Abs_point2d(0,0),Abs_point2d(2,1))"
+definition "t2 \<equiv> Abs_trapez ((Abs_point2d(1,3),Abs_point2d(4,4)),(Abs_point2d(2,1),Abs_point2d(3,0)),Abs_point2d(2,1),Abs_point2d(3,0))"
+definition "t3 \<equiv> Abs_trapez ((Abs_point2d(2,1),Abs_point2d(3,0)),(Abs_point2d(0,0),Abs_point2d(3,0)),Abs_point2d(2,1),Abs_point2d(3,0))"
+definition "t4 \<equiv> Abs_trapez ((Abs_point2d(1,3),Abs_point2d(4,4)),(Abs_point2d(0,0),Abs_point2d(3,0)),Abs_point2d(3,0),Abs_point2d(4,4))"
+lemma  "replaceTrapez (Tip t0) t0 [t0] (Abs_point2d(2,1)) (Abs_point2d(3,0)) =
+  Node (Tip t1) (xNode (Abs_point2d(2,1))) (Node (Node (Tip t2) (yNode ((Abs_point2d(2,1)),(Abs_point2d(3,0)))) (Tip t3)) (xNode (Abs_point2d(3,0))) (Tip t4))"
+  apply (simp add: replaceTrapez_def)
+  apply (simp only: replaceTrapezSimp_def)
+  apply (simp only: t0_def t1_def t2_def t3_def t4_def leftPSimp rightPSimp topTSimp bottomTSimp)
+  apply (simp)
+  apply (simp add: replaceTrapezSimpQ_def replaceTrapezSimpA_def)
+done
 end
