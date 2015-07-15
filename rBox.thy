a9d2fd471b511472a4f51b0bbea465d4ec3d0455
Viktor Befort <v.befort@googlemail.com>
2015-07-15 18:29:27 +0200

    add pointLists definition

--- rBox.thy
+++ rBox.thy
@@ -5,9 +5,12 @@ begin
 (*es gibt kein Punkt in der Punkt Liste, mit gleicher xCoordinate*)
 definition uniqueXCoord :: "point2d list \<Rightarrow> bool" where
   "uniqueXCoord L \<equiv> \<forall> a b. a \<noteq> b \<longrightarrow> xCoord (L!a) \<noteq> xCoord (L!b)"
+lemma "3 \<le> length L \<Longrightarrow> uniqueXCoord L \<Longrightarrow> pointList (L)"
+  apply (simp add: uniqueXCoord_def pointList_def)
+by (metis distinct_conv_nth)
 
 (*4eckige Box um pointListen herum ist selbst eine pointList*)
-lemma rBoxPointList: "pointList (concat PL) \<Longrightarrow> pointList(
+lemma rBoxPointList: "pointLists PL \<Longrightarrow> pointList(
   [Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
@@ -16,14 +19,14 @@ sorry
 
 (*4eckige Box um pointListen herum*)
 definition rBox :: "(point2d list) list \<Rightarrow> point2d list" where
-  "pointList (concat PL) \<Longrightarrow> uniqueXCoord (concat PL) \<Longrightarrow> rBox PL \<equiv>
+  "pointLists PL \<Longrightarrow> uniqueXCoord (concat PL) \<Longrightarrow> rBox PL \<equiv>
   cyclePath([Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
   Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1,yCoord (last (yCoordSort (concat PL))) + 1)])"
 
 (*ersetzte den Term Polygon im Satz*)
-lemma rBoxPoly [simp] : "pointList (concat PL) \<Longrightarrow>
+lemma rBoxPoly [simp] : "pointLists PL \<Longrightarrow>
   cyclePath([Abs_point2d(xCoord (hd (xCoordSort (concat PL))) - 1, yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (hd (yCoordSort (concat PL))) - 1),
   Abs_point2d(xCoord (last (xCoordSort (concat PL))) + 1,yCoord (last (yCoordSort (concat PL))) + 1),
@@ -38,11 +41,11 @@ lemma rBoxPoly [simp] : "pointList (concat PL) \<Longrightarrow>
 done
 
 (*rBox ist ein Convexes Polygon*)
-lemma rBoxConvex : "pointList (concat PL) \<Longrightarrow> polygon (rBox PL)"  
+lemma rBoxConvex : "pointLists PL \<Longrightarrow> polygon (rBox PL)"  
 oops
 
 (*alle Punkte von PL sind innerhalb von rBox PL*)
-lemma "pointList (concat PL) \<Longrightarrow> \<forall> a \<in> set L. pointInsidePolygon (rBox PL) a"
+lemma "pointLists PL \<Longrightarrow> \<forall> a \<in> set L. pointInsidePolygon (rBox PL) a"
   apply (cut_tac PL=PL in rBoxConvex, assumption)
 oops
 
